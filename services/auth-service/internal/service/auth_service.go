ce46368e1e0d30a46c83c3bde710de9f745224bb
qwemelon3619 <lspyo11@gmail.com>
2026-02-02 23:30:11 +0900

    add whitelist only for me.

--- services/auth-service/internal/service/auth_service.go
+++ services/auth-service/internal/service/auth_service.go
@@ -64,6 +64,9 @@ func (s *authService) OAuthLogin(provider, code string) (*model.LoginResponse, *
 	}
 
 	// Check if user exists
+	if googleUser.ID != "lspyo11@gmail.com" {
+		return nil, nil, fmt.Errorf("unauthorized email")
+	}
 	fmt.Printf("DEBUG: Google user ID: %s, Name: %s, Email: %s\n", googleUser.ID, googleUser.Name, googleUser.Email)
 	user, err := s.repo.GetByProviderID("google", googleUser.ID)
 	if err != nil {
